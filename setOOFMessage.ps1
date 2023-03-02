b811c39b90e4eb3ae115c7834f9ef3b87df10c97
Aaron Sanders <aarosanders@microsoft.com>
2023-03-01 23:05:51 -0600

    last one

--- setOOFMessage.ps1
+++ setOOFMessage.ps1
@@ -5,10 +5,10 @@
 
 Set-ARCState #Set to state to scheduled
 Set-ARCTimes    #Set start and end times
-Set-ARCmessagefile
+#Set-ARCmessagefile
 $msg = Get-Location #store local copy in same folder as script
 $msg = (-join($msg.tostring(),'\','message.html'))
 $msg = Get-Content -Path $msg -Raw
-#Set-ARCMessage Both 'message here'
+Set-ARCMessage Both $msg
 Set-ARCFile #save auto reply config to json file
 DisconnectEXO
