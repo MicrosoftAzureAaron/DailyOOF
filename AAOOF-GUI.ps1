8b6b5be035cfe27402c286e63dc393809f226f68
Aaron Sanders <aarosanders@microsoft.com>
2026-04-16 13:58:09 -0500

    1.5.0 config updates

--- AAOOF-GUI.ps1
+++ AAOOF-GUI.ps1
@@ -42,7 +42,7 @@ if (!(Test-Path $ConfigDir)) { New-Item -ItemType Directory -Path $ConfigDir | O
 # so that the tool works out of the box without manual file setup.
 $RepoBaseUrl = "https://raw.githubusercontent.com/MicrosoftAzureAaron/DailyOOF/main/config"
 $ScriptUpdateUrl = "https://raw.githubusercontent.com/MicrosoftAzureAaron/DailyOOF/main/AAOOF-GUI.ps1"
-$script:ScriptVersion = "1.4.6"
+$script:ScriptVersion = "1.5.0"
 $DefaultConfigFiles = @(
     "AAOOF-GUI.xaml",
     "normal_oof.html",
